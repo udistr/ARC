cddb4a1a00d8c63b6c9086788b6229d807209bc9
udistr <udistr@gmail.com>
2019-11-19 22:32:45 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ARC',
+      version='0.1',
+      description='Asymmetric Range Calibration',
+      url='https://github.com/udistr/SLA.git',
+      author='Ehud Strobach',
+      author_email='udistr@gmail.com',
+      license='MIT',
+      packages=['ARC'],
+      zip_safe=False)
